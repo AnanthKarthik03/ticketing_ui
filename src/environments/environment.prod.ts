e519f06b33e5c7039d450d996444bf9135ba14a0
200520907 <200520907@student.georgianc.on.ca>
2022-09-07 01:09:04 -0400

    UAT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,9 +9,17 @@ export const environment = {
 
   // Rostan Live
 
-  apiUrl: 'https://rstapi.rostantechnologies.com:9990/',
-  company_logo: 'http://129.154.38.81/ticketing_tool/rostan_server/company_logo/',
-  employee_logo: 'http://129.154.38.81/ticketing_tool/rostan_server/employee_logo/',
-  customer_logo: 'http://129.154.38.81/ticketing_tool/rostan_server/customer_logo/',
-  ticketingFiles: 'http://129.154.38.81/ticketing_tool/rostan_server/ticketingFiles/',
+  // apiUrl: 'https://rstapi.rostantechnologies.com:9990/',
+  // company_logo: 'http://129.154.38.81/ticketing_tool/rostan_server/company_logo/',
+  // employee_logo: 'http://129.154.38.81/ticketing_tool/rostan_server/employee_logo/',
+  // customer_logo: 'http://129.154.38.81/ticketing_tool/rostan_server/customer_logo/',
+  // ticketingFiles: 'http://129.154.38.81/ticketing_tool/rostan_server/ticketingFiles/',
+
+  // Rostan Live
+
+  apiUrl: 'https://rstapi.rostantechnologies.com:9992/',
+  company_logo: 'http://129.154.38.81/ticketing_tool/rostan_server_uat/company_logo/',
+  employee_logo: 'http://129.154.38.81/ticketing_tool/rostan_server_uat/employee_logo/',
+  customer_logo: 'http://129.154.38.81/ticketing_tool/rostan_server_uat/customer_logo/',
+  ticketingFiles: 'http://129.154.38.81/ticketing_tool/rostan_server_uat/ticketingFiles/',
 };
